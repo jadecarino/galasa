90001a2febe751781bf747a38d7ada14645ea1a0
techcobweb <77053+techcobweb@users.noreply.github.com>
2025-08-04 09:12:34 +0100

    TestSleep IVT improved to add multiple methods, each sleeping a small bit
    Signed-off-by: techcobweb <77053+techcobweb@users.noreply.github.com>

--- modules/ivts/galasa-ivts-parent/dev.galasa.ivts/dev.galasa.ivts.core/src/main/java/dev/galasa/ivts/core/TestSleep.java
+++ modules/ivts/galasa-ivts-parent/dev.galasa.ivts/dev.galasa.ivts.core/src/main/java/dev/galasa/ivts/core/TestSleep.java
@@ -12,7 +12,7 @@ import dev.galasa.Test;
 import dev.galasa.core.manager.Logger;
 
 @Test
-@Summary("A basic test with a sleep so there is sufficient time to test the `galasactl runs reset` command")
+@Summary("A basic test with a sleep so there is sufficient time to test the `galasactl runs reset` and `galasactl runs cancel` commands")
 public class TestSleep {
 
     @Logger
@@ -20,7 +20,25 @@ public class TestSleep {
 
     @Test
     public void sleep() throws Exception {
-        logger.info("Sleeping for 1-minute.");
+        logger.info("Sleeping for 30 seconds.");
+        Thread.sleep(60000);
+    }
+
+    @Test
+    public void sleep2() throws Exception {
+        logger.info("Sleeping for another 30 seconds");
+        Thread.sleep(60000);
+    }
+
+    @Test
+    public void sleep3() throws Exception {
+        logger.info("Sleeping for yet another 30 seconds");
+        Thread.sleep(60000);
+    }
+
+    @Test
+    public void sleep4() throws Exception {
+        logger.info("Sleeping for a final 30 seconds");
         Thread.sleep(60000);
     }
 
